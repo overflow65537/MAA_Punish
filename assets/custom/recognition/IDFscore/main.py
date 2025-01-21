5a6488293a1623b0a8965e46210b442f665dd31b
overflow <err.overflow@gmail.com>
2025-01-21 17:30:11 +0800

    fix:修复识别分数返回错误的值

--- assets/custom/recognition/IDFscore/main.py
+++ assets/custom/recognition/IDFscore/main.py
@@ -15,10 +15,12 @@ class IDFscore(CustomRecognition):
         # 检查当前分数
         current_score = context.run_recognition("检查当前分数", image)
         if current_score is None or target_score is None:
-            return None
-        if int(current_score.best_result.text) >= int(target_score.best_result.text):
-            return CustomRecognition.AnalyzeResult(
-                box=(0, 0, 100, 100), detail="success"
-            )
+            return
+        if current_score.best_result.text.isdigit() and target_score.best_result.text.isdigit():
+            
+            if int(current_score.best_result.text) >= int(target_score.best_result.text):
+                return CustomRecognition.AnalyzeResult(
+                    box=(0, 0, 100, 100), detail="success"
+                )
         else:
-            return None
+            return
