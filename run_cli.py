f3f69b574545906de8af9c211e11fcc0e21f1e9a
overflow <err.overflow@gmail.com>
2025-01-27 16:22:32 +0800

    feat: 添加-d运行方式

--- run_cli.py
+++ run_cli.py
@@ -8,8 +8,12 @@ from assets.custom.action.ScreenShot.main import ScreenShot
 from assets.custom.action.Identify.main import Identify
 from assets.custom.action.ResetIdentify.main import ResetIdentify
 from assets.custom.action.CenterCamera.main import CenterCamera
+from assets.custom.recognition.CalculateScore.main import CalculateScore
+from assets.custom.recognition.IDFMembers.main import IDFMembers
+from assets.custom.recognition.IDFscore.main import IDFscore
 from maa.toolkit import Toolkit
 
+import sys
 
 def main():
     # 注册自定义动作
@@ -23,10 +27,13 @@ def main():
     Toolkit.pi_register_custom_action("Identify", Identify())
     Toolkit.pi_register_custom_action("ResetIdentify", ResetIdentify())
     Toolkit.pi_register_custom_action("CenterCamera", CenterCamera())
+    # 注册自定义识别
+    Toolkit.pi_register_custom_recognition("CalculateScore", CalculateScore())
+    Toolkit.pi_register_custom_recognition("IDFMembers", IDFMembers())  
+    Toolkit.pi_register_custom_recognition("IDFscore", IDFscore())
 
-    # 启动 MaaPiCli
-    Toolkit.pi_run_cli("./", "./", False)
-
+    directly = "-d" in sys.argv
+    Toolkit.pi_run_cli("./", "./", directly)
 
 if __name__ == "__main__":
     main()
