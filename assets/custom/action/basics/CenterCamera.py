aefcefc541cca5d800d52ee804a4bc4aad18c601
overflow <err.overflow@gmail.com>
2025-02-14 03:57:06 +0800

    refactor:重构寒境曙光任务

--- assets/custom/action/basics/CenterCamera.py
+++ assets/custom/action/basics/CenterCamera.py
@@ -1,5 +1,6 @@
 from maa.context import Context
 from maa.custom_action import CustomAction
+import time
 
 
 class CenterCamera(CustomAction):
@@ -7,11 +8,26 @@ class CenterCamera(CustomAction):
         self, context: Context, argv: CustomAction.RunArg
     ) -> CustomAction.RunResult:
         image=context.tasker.controller.post_screencap().wait().get()
-        origin_pos=context.run_recognition("回到boss界面_操作",image)
-        x, y = (
-                    origin_pos.best_result.box[0],
-                    origin_pos.best_result.box[1],
-                )
-        context.tasker.controller.post_swipe(x, y, 1052, 342, 2000).wait()
-        
-        return CustomAction.RunResult(success=True)
+        if argv.custom_action_param == '{"tower":true}':
+            origin_pos=context.run_recognition("战斗地块_寒境曙光",image)
+            x, y = (
+                        origin_pos.best_result.box[0],
+                        origin_pos.best_result.box[1],
+                    )
+            context.tasker.controller.post_swipe(x, y, 764,334, 2000).wait()
+            time.sleep(1)
+            context.tasker.controller.post_click(100,100).wait()
+            time.sleep(1)
+            return CustomAction.RunResult(success=True)
+        else:
+            origin_pos=context.run_recognition("重置镜头",image)
+            x, y = (
+                        origin_pos.best_result.box[0],
+                        origin_pos.best_result.box[1],
+                    )
+            context.tasker.controller.post_swipe(x, y, 1052, 342, 2000).wait()
+            time.sleep(1)
+            context.tasker.controller.post_click(100,100).wait()
+            time.sleep(1)
+            return CustomAction.RunResult(success=True)
+
