e3c40304deeae5173245e61442d4d78e1953a4c1
chongxuan-huang <chongxuan-huang@outlook.com>
2025-02-24 19:31:04 +0800

    dev:暂定通用战斗类

---
+++ assets/custom/action/basics/CombatActions.py
@@ -0,0 +1,19 @@
+class CombatActions:
+    def __init__(self):
+        """初始化"""
+        self.name = "CombatActions"
+
+    def attack(self):
+        """攻击"""
+
+    def dodge(self):
+        """闪避"""
+
+    def use_skill(self):
+        """技能"""
+
+    def consume_resource(self):
+        """消球"""
+
+    def trigger_qte(self):
+        """触发QTE"""
