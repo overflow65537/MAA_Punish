341a54c5e6f79c0a7386c47c30a50ab02900b5fb
overflow <err.overflow@gmail.com>
2025-03-08 22:29:43 +0800

    feat: 添加自定义代理和主程序文件以支持dbg调试

---
+++ assets/custom/agent_file.py
@@ -0,0 +1,101 @@
+from maa.agent.agent_server import AgentServer
+from action.basics.CenterCamera import CenterCamera
+from action.basics.CombatActions import CombatActions
+from action.basics.Identify import Identify
+from action.basics.IdentifyRoles import IdentifyRoles
+from action.basics.MultiplayerAutoBattle import MultiplayerAutoBattle
+from action.basics.ResetIdentify import ResetIdentify
+from action.basics.ScreenShot import ScreenShot
+from action.basics.SelectCharacter import SelectCharacter
+
+
+from action.exclusives.CrimsonWeave import CrimsonWeave
+from action.exclusives.LostLullaby import LostLullaby
+from action.exclusives.Oblivion import Oblivion
+from action.exclusives.Pyroath import Pyroath
+from action.exclusives.Stigmata import Stigmata
+
+
+from recognition.exclusives.CalculateScore import CalculateScore
+from recognition.exclusives.IDFMembers import IDFMembers
+from recognition.exclusives.IDFscore import IDFscore
+
+
+@AgentServer.custom_action("CenterCamera")
+class Agent_CenterCamera(CenterCamera):
+    pass
+
+
+@AgentServer.custom_action("CombatActions")
+class Agent_CombatActions(CombatActions):
+    pass
+
+
+@AgentServer.custom_action("Identify")
+class Agent_Identify(Identify):
+    pass
+
+
+@AgentServer.custom_action("IdentifyRoles")
+class Agent_IdentifyRoles(IdentifyRoles):
+    pass
+
+
+@AgentServer.custom_action("MultiplayerAutoBattle")
+class Agent_MultiplayerAutoBattle(MultiplayerAutoBattle):
+    pass
+
+
+@AgentServer.custom_action("ResetIdentify")
+class Agent_ResetIdentify(ResetIdentify):
+    pass
+
+
+@AgentServer.custom_action("ScreenShot")
+class Agent_ScreenShot(ScreenShot):
+    pass
+
+
+@AgentServer.custom_action("SelectCharacter")
+class Agent_SelectCharacter(SelectCharacter):
+    pass
+
+
+@AgentServer.custom_recognition("CalculateScore")
+class Agent_CalculateScore(CalculateScore):
+    pass
+
+
+@AgentServer.custom_recognition("IDFMembers")
+class Agent_IDFMembers(IDFMembers):
+    pass
+
+
+@AgentServer.custom_recognition("IDFscore")
+class Agent_IDFscore(IDFscore):
+    pass
+
+
+@AgentServer.custom_action("CrimsonWeave")
+class Agent_CrimsonWeave(CrimsonWeave):
+    pass
+
+
+@AgentServer.custom_action("LostLullaby")
+class Agent_LostLullaby(LostLullaby):
+    pass
+
+
+@AgentServer.custom_action("Oblivion")
+class Agent_Oblivion(Oblivion):
+    pass
+
+
+@AgentServer.custom_action("Pyroath")
+class Agent_Pyroath(Pyroath):
+    pass
+
+
+@AgentServer.custom_action("Stigmata")
+class Agent_Stigmata(Stigmata):
+    pass
---
+++ assets/custom/main.py
@@ -0,0 +1,22 @@
+import sys
+from maa.agent.agent_server import AgentServer
+from maa.toolkit import Toolkit
+
+from agent_file import *
+
+
+def main():
+    Toolkit.init_option("./")
+    if len(sys.argv) > 1:
+        print("使用自定义socket_id: " + sys.argv[-1])
+        socket_id = sys.argv[-1]
+    else:
+        print("使用默认socket_id: MAA_AGENT_SOCKET")
+        socket_id = "MAA_AGENT_SOCKET"
+    AgentServer.start_up(socket_id)
+    AgentServer.join()
+    AgentServer.shut_down()
+
+
+if __name__ == "__main__":
+    main()
