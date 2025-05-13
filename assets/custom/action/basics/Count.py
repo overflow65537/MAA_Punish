04e45964368577390dfc9f2c6a0dc22d9d68a74c
overflow <err.overflow@gmail.com>
2025-05-13 21:59:06 +0800

    refactor: 优化Count类的run方法，改进参数处理和节点执行逻辑

--- assets/custom/action/basics/Count.py
+++ assets/custom/action/basics/Count.py
@@ -7,33 +7,55 @@ class Count(CustomAction):
     def run(
         self, context: Context, argv: CustomAction.RunArg
     ) -> CustomAction.RunResult:
-        argv: dict = json.loads(argv.custom_action_param)
-        print(argv)
-        if not argv:
+        """
+        自定义动作：
+        custom_action_param:
+            {
+                "count": 0,
+                "target_count": 10,
+                "next_node": ["node1", "node2"],
+                "else_node": ["node3"],
+            }
+        count: 当前次数
+        target_count: 目标次数
+        next_node: 达到目标次数后执行的节点. 支持多个节点，按顺序执行，可以出现重复节点，可以为空
+        else_node: 未达到目标次数时执行的节点. 支持多个节点，按顺序执行，可以出现重复节点，可以为空
+        """
+
+        argv_dict: dict = json.loads(argv.custom_action_param)
+        print(argv_dict)
+        if not argv_dict:
             return CustomAction.RunResult(success=True)
-        if argv.get("count") < argv.get("target_count"):
-            argv["count"] += 1
-            context.override_pipeline(
-                {
-                    argv.get("self"): {
-                        "custom_action_param": argv,
-                    },
-                }
-            )
+        
+        current_count = argv_dict.get("count", 0)
+        target_count = argv_dict.get("target_count", 0)
+        
+        if current_count <= target_count:
+            argv_dict["count"] = current_count + 1
+            context.override_pipeline({
+                argv.node_name: {"custom_action_param": argv_dict}
+            })
+            self._run_nodes(context, argv_dict.get("else_node"))
         else:
-            context.override_pipeline(
-                {
-                    argv.get("self"): {
-                        "custom_action_param": {
-                            "self": argv.get("self"),
-                            "count": 0,
-                            "target_count": argv.get("target_count"),
-                            "next_node": argv.get("next_node"),
-                        },
-                    },
+            context.override_pipeline({
+                argv.node_name: {
+                    "custom_action_param": {
+                        "count": 0,
+                        "target_count": target_count,
+                        "else_node": argv_dict.get("else_node"),
+                        "next_node": argv_dict.get("next_node")
+                    }
                 }
-            )
-            for i in argv.get("next_node"):
-                context.run_task(i)
+            })
+            self._run_nodes(context, argv_dict.get("next_node"))
 
         return CustomAction.RunResult(success=True)
+
+    def _run_nodes(self, context: Context, nodes):
+        """统一处理节点执行逻辑"""
+        if not nodes:
+            return
+        if isinstance(nodes, str):
+            nodes = [nodes]
+        for node in nodes:
+            context.run_task(node)
