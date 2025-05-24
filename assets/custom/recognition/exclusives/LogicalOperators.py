c400053668788f1910381699bdc32c4ab2483c36
overflow65537 <err.overflow@gmail.com>
2025-05-24 20:34:21 +0800

    refactor:重构囚笼 (#72)
    * feat: 添加逻辑识别器LOp及相关配置
    
    * 重构完成

---
+++ assets/custom/recognition/exclusives/LogicalOperators.py
@@ -0,0 +1,55 @@
+from maa.context import Context
+from maa.custom_recognition import CustomRecognition
+import json
+
+
+class LOp(CustomRecognition):
+    def analyze(
+        self,
+        context: Context,
+        argv: CustomRecognition.AnalyzeArg,
+    ) -> CustomRecognition.AnalyzeResult:
+        """
+        逻辑识别器：
+        custom_recognition_param:
+            {
+                "mode": and,
+                "nodes": ["node1", ["node2"]],
+            }
+        mode: 模式 and 或者 or,默认为and
+        nodes: 需要识别的节点,使用列表括起来为反转识别结果
+        """
+        image = argv.image
+        param: dict = json.loads(argv.custom_recognition_param)
+        mode: str = param.get("mode", "and")
+        nodes: list = param.get("nodes", [])
+
+        if mode == "and":
+            for item in nodes:
+                result = self._eval_node(item, context, image)
+                if not result:
+                    return
+            return CustomRecognition.AnalyzeResult(
+                box=(0, 0, 100, 100), detail=f"{nodes} used in {mode} success"
+            )
+
+        elif mode == "or":
+            for item in nodes:
+                result = self._eval_node(item, context, image)
+                if result:
+                    return CustomRecognition.AnalyzeResult(
+                        box=(0, 0, 100, 100), detail=f"{nodes} used in {mode} success"
+                    )
+            return
+
+        else:
+            return
+
+    def _eval_node(self, node, context: Context, image) -> bool:
+
+        if isinstance(node, str):
+            return bool(context.run_recognition(node, image))
+
+        elif isinstance(node, list) and len(node) == 1:
+            inner_node = node[0]
+            return not bool(context.run_recognition(inner_node, image))
