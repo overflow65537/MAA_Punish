db32f07e33ca62f31eeab4fdc1e589822273964a
overflow65537 <err.overflow@gmail.com>
2025-06-10 16:40:10 +0800

    feat: 更新run_cli.py以支持自定义对象加载和调试日志记录

--- run_cli.py
+++ run_cli.py
@@ -1,75 +1,133 @@
-import sys
 import os
-
 if not os.path.exists("run_cli.py"):
     os.environ["MAAFW_BINARY_PATH"] = os.getcwd()
-
 from maa.toolkit import Toolkit
 
-from assets.custom.action.basics.CenterCamera import CenterCamera
-from assets.custom.action.basics.CombatActions import CombatActions
-from assets.custom.action.basics.Identify import Identify
-from assets.custom.action.basics.IdentifyRoles import IdentifyRoles
-from assets.custom.action.basics.MultiplayerAutoBattle import MultiplayerAutoBattle
-from assets.custom.action.basics.ResetIdentify import ResetIdentify
-from assets.custom.action.basics.ScreenShot import ScreenShot
-from assets.custom.action.basics.SetTower import SetTower
-from assets.custom.action.basics.Count import Count
-from assets.custom.action.basics.PPOverride import PPOverride
-from assets.custom.action.basics.ChainLoopCircuit import ChainLoopCircuit
+from maa.context import Context
+from maa.custom_action import CustomAction
+from maa.custom_recognition import CustomRecognition
 
-from assets.custom.action.exclusives.CrimsonWeave import CrimsonWeave
-from assets.custom.action.exclusives.LostLullaby import LostLullaby
-from assets.custom.action.exclusives.Oblivion import Oblivion
-from assets.custom.action.exclusives.Pyroath import Pyroath
-from assets.custom.action.exclusives.Stigmata import Stigmata
-from assets.custom.action.exclusives.Shukra import Shukra
-
-from assets.custom.recognition.exclusives.CalculateScore import CalculateScore
-from assets.custom.recognition.exclusives.IDFMembers import IDFMembers
-from assets.custom.recognition.exclusives.IDFscore import IDFscore
-from assets.custom.recognition.exclusives.IDFMasteryLevel import IDFMasteryLevel
-from assets.custom.recognition.exclusives.LogicalOperators import LOp
-
-print("如无必要,请使用MFW.exe运行")
+import sys
+print("如无必要，请使用MFW.exe运行")
 print("if not necessary, please use MFW.exe to run")
+#创建一个logger
+import logging
+import os
+import importlib.util
+from typing import Dict
+import logging
+import os
+from logging.handlers import TimedRotatingFileHandler
 
+
+
+def Read_Config(file_path):
+    """
+    读取配置文件
+    """
+    import json
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        config = json.load(f)
+    return config
+
+def path_to_list(path):
+    """将路径转换为列表形式"""
+    parts = []
+    while True:
+        path, part = os.path.split(path)
+        if part:
+            parts.insert(0, part)
+        else:
+            if path:
+                parts.insert(0, path)
+            break
+    return parts
+
+
+os.makedirs("debug", exist_ok=True)
+logging.basicConfig(
+    format="[%(asctime)s][%(levelname)s][%(filename)s][L%(lineno)d][%(funcName)s] | %(message)s",
+    level=logging.DEBUG,
+    handlers=[
+        TimedRotatingFileHandler(
+            os.path.join(os.getcwd(), "debug", "picli.log"),
+            when="midnight",
+            backupCount=3,
+            encoding="utf-8",
+        ),
+        logging.StreamHandler(),
+    ],
+)
+logger = logging.getLogger(__name__)
+
+
+
+def load_custom_objects(custom_dir):
+
+    if not os.path.exists(custom_dir):
+        logger.warning(f"自定义文件夹 {custom_dir} 不存在")
+        return
+    if not os.listdir(custom_dir):
+        logger.warning(f"自定义文件夹 {custom_dir} 为空")
+        return
+    if os.path.exists(os.path.join(custom_dir, "custom.json")):
+        logger.info("配置文件方案")
+        custom_config: Dict[str, Dict] = Read_Config(
+            os.path.join(custom_dir, "custom.json")
+        )
+        for custom_name, custom in custom_config.items():
+            custom_type: str = custom.get("type", "")
+            custom_class_name: str = custom.get("class", "")
+            custom_file_path: str = custom.get("file_path", "")
+            if "{custom_path}" in custom_file_path:
+                custom_file_path = custom_file_path.replace(
+                    "{custom_path}", custom_dir
+                )
+                custom_file_path = os.path.join(*path_to_list(custom_file_path))
+
+            if not all(
+                [custom_type, custom_name, custom_class_name, custom_file_path]
+            ):
+                logger.warning(f"配置项 {custom} 缺少必要信息，跳过")
+                continue
+            print(
+                f"custom_type: {custom_type}, custom_name: {custom_name}, custom_class_name: {custom_class_name}, custom_file_path: {custom_file_path}"
+            )
+            module_name = os.path.splitext(os.path.basename(custom_file_path))[0]
+            # 动态导入模块
+            spec = importlib.util.spec_from_file_location(
+                module_name, custom_file_path
+            )
+            if spec is None:
+                logger.error(f"无法获取模块 {module_name} 的 spec，跳过加载")
+                continue
+            module = importlib.util.module_from_spec(spec)
+
+            if spec.loader is None:
+                logger.error(f"模块 {module_name} 的 loader 为 None，跳过加载")
+                continue
+            spec.loader.exec_module(module)
+            print(f"模块 {module} 导入成功")
+
+            # 获取类对象
+            class_obj = getattr(module, custom_class_name)
+
+            # 实例化类
+            instance = class_obj()
+
+            if custom_type == "action":
+                if Toolkit.pi_register_custom_action(custom_name, instance):
+                    logger.info(f"加载自定义动作{custom_name}")
+            elif custom_type == "recognition":
+                if Toolkit.pi_register_custom_recognition(custom_name, instance):
+                    logger.info(f"加载自定义识别器{custom_name}")
 
 def main():
-    print("开始注册自定义内容")
-    # 注册自定义动作-角色战斗逻辑
-    Toolkit.pi_register_custom_action("CrimsonWeave", CrimsonWeave())  # 深红囚影
-    Toolkit.pi_register_custom_action("LostLullaby", LostLullaby())  # 深谣
-    Toolkit.pi_register_custom_action("Oblivion", Oblivion())  # 终焉
-    Toolkit.pi_register_custom_action("Pyroath", Pyroath())  # 誓焰
-    Toolkit.pi_register_custom_action("Stigmata", Stigmata())  # 深痕
-    Toolkit.pi_register_custom_action("Shukra", Shukra())  # 启明
-    # 注册自定义动作-通用逻辑
-    Toolkit.pi_register_custom_action("ScreenShot", ScreenShot())  # 错误截图
-    Toolkit.pi_register_custom_action("Identify", Identify())  # 识别人物
-    Toolkit.pi_register_custom_action("ResetIdentify", ResetIdentify())  # 重置识别
-    Toolkit.pi_register_custom_action("CenterCamera", CenterCamera())  # 重置镜头
-    Toolkit.pi_register_custom_action("IdentifyRoles", IdentifyRoles())  # 角色识别
-    Toolkit.pi_register_custom_action(
-        "MultiplayerAutoBattle", MultiplayerAutoBattle()
-    )  # 多人战斗
-    Toolkit.pi_register_custom_action("CombatActions", CombatActions())  # 通用角色战斗
-    Toolkit.pi_register_custom_action("SetTower", SetTower())  # 设置塔
-    Toolkit.pi_register_custom_action("Count", Count())  # 计数
-    Toolkit.pi_register_custom_action("PPOverride", PPOverride())  # 覆写
-    Toolkit.pi_register_custom_action(
-        "ChainLoopCircuit", ChainLoopCircuit()
-    )  # 链合回路
+    # 注册自定义动作
+    load_custom_objects(os.path.join(os.getcwd(), "custom"))
+
     # 注册自定义识别
-    Toolkit.pi_register_custom_recognition(
-        "CalculateScore", CalculateScore()
-    )  # 计算分数
-    Toolkit.pi_register_custom_recognition("IDFMembers", IDFMembers())  # 识别宿舍成员
-    Toolkit.pi_register_custom_recognition("IDFscore", IDFscore())  # 识别分数
-    Toolkit.pi_register_custom_recognition(
-        "IDFMasteryLevel", IDFMasteryLevel()
-    )  # 识别精通等级
-    Toolkit.pi_register_custom_recognition("LOp", LOp())  # 逻辑识别器
 
     directly = "-d" in sys.argv
     Toolkit.pi_run_cli("./", "./", directly)
