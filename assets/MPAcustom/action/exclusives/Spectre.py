f848a5ed874fbd9d784d66bad5ededc40261aefc
overflow65537 <err.overflow@gmail.com>
2026-01-01 13:36:52 +0800

    perf:优化骇影战斗逻辑

--- assets/MPAcustom/action/exclusives/Spectre.py
+++ assets/MPAcustom/action/exclusives/Spectre.py
@@ -22,45 +22,54 @@ class Spectre(CustomAction):
             # 红球
             print("开始消红球")
             for _ in range(30):
-                actions.ball_elimination_target(2)  # 消球
-                time.sleep(0.05)
-                actions.ball_elimination_target(1)
-                time.sleep(0.05)
-                actions.auto_qte("r")
+                if actions.check_status("检查骇影2阶段"):
+                    actions.ball_elimination_target(2)  # 消球
+                    time.sleep(0.05)
+                    actions.ball_elimination_target(1)
+                    time.sleep(0.05)
             time.sleep(0.1)
 
             # 黄球
             print("开始消黄球")
             for _ in range(20):
-                actions.attack()
-                time.sleep(0.05)
-                actions.ball_elimination_target(1)
-                time.sleep(0.05)
+                if actions.check_status("检查骇影2阶段"):
+                    actions.attack()
+                    time.sleep(0.05)
+                    actions.ball_elimination_target(1)
+                    time.sleep(0.05)
 
             # 蓝球
             print("开始消蓝球")
             actions.long_press_attack(1000)
             for _ in range(10):
-                actions.ball_elimination_target(1)
-                time.sleep(0.05)
-                actions.attack()
-                time.sleep(0.05)
+                if actions.check_status("检查骇影2阶段"):
+                    actions.ball_elimination_target(1)
+                    time.sleep(0.05)
+                    actions.attack()
+                    time.sleep(0.05)
 
         elif actions.count_signal_balls() >= 9:
             print("一阶段")
-            for _ in range(20):
+            item = 0
+            while not actions.check_Skill_energy_bar() and item < 100:
                 actions.ball_elimination_target(2)  # 消球
                 time.sleep(0.05)
                 actions.ball_elimination_target(1)
                 time.sleep(0.05)
                 actions.attack()
                 time.sleep(0.05)
+                item += 1
 
-            if actions.check_Skill_energy_bar():
-                actions.use_skill()
-                time.sleep(0.5)
+            actions.use_skill()
+            time.sleep(0.5)
         else:
             print("一阶段信号球不足")
-            actions.continuous_attack(8, 100)
+            item = 0
+
+            while not actions.check_status("检查骇影2阶段") and item < 100:
+                actions.attack()
+                print(bool(actions.check_status("检查骇影2阶段")))
+                time.sleep(0.05)
+                item += 1
 
         return CustomAction.RunResult(success=True)
