4c1c84bf5ee7ea149884a6ffe585056a542546ff
overflow65537 <err.overflow@gmail.com>
2026-03-02 18:54:06 +0800

    chore:i18n准备

--- configure.py
+++ configure.py
@@ -7,11 +7,18 @@ assets_dir = Path(__file__).parent / "assets"
 
 def configure_ocr_model():
     shutil.copytree(
-        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
+        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v5" / "zh_cn",
         assets_dir / "resource" / "base" / "model" / "ocr",
         dirs_exist_ok=True,
     )
-
+    rm_dir = assets_dir / "resource" / "base" / "model" / "ocr" / "det.onnx"
+    if rm_dir.exists():
+        rm_dir.unlink()
+    shutil.copytree(
+        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn"/"det.onnx",
+        assets_dir / "resource" / "base" / "model" / "ocr"/"det.onnx",
+        dirs_exist_ok=True,
+    )
 
 if __name__ == "__main__":
     configure_ocr_model()
