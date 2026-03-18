882cd2ac6b87402f243117b3b76c50da87b384c5
overflow65537 <err.overflow@gmail.com>
2026-03-18 22:07:26 +0800

    perf:替换检测模型

--- configure.py
+++ configure.py
@@ -11,13 +11,13 @@ def configure_ocr_model():
         assets_dir / "resource" / "base" / "model" / "ocr",
         dirs_exist_ok=True,
     )
-    """rm_dir = assets_dir / "resource" / "base" / "model" / "ocr" / "det.onnx"
+    rm_dir = assets_dir / "resource" / "base" / "model" / "ocr" / "det.onnx"
     if rm_dir.exists():
         rm_dir.unlink()
     shutil.copyfile(
         assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn"/"det.onnx",
         assets_dir / "resource" / "base" / "model" / "ocr"/"det.onnx",
-    )"""
+    )
 
 if __name__ == "__main__":
     configure_ocr_model()
